a9319ee9a046b5d0153af730818f2fb4af587f7a
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2024-12-12 23:04:40 +0100

    fixed stuff

--- Assets/ballsmm.cs
+++ Assets/ballsmm.cs
@@ -7,15 +7,7 @@ public class ballsmm : MonoBehaviour
 {
     public int damage = 1;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Health health = collision.gameObject.GetComponent<Health>();
-
-        if (health != null)
-        {
-            health.TakeDamage(damage); //apply damage if object has health
-        }
-    }
+    
 
     Rigidbody2D myRigidbody;
     public float speed = 40f;
